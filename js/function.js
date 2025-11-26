2a8ff7312273cf43597335d637fba5c2ab0532c3
shifat-it <halimshifat@gmail.com>
2025-11-26 18:16:26 +0600

    Initial project upload

---
+++ js/function.js
@@ -0,0 +1,20 @@
+/* ----------------------------------------------------------------------------------------
+* Author        : B.M.Ohedul Islam
+* Template Name : Under Maintenance
+* File          : Custom JS
+* Version       : 1.0
+* ---------------------------------------------------------------------------------------- */
+(function ($) {
+    "use strict";
+	
+	var $window = $(window); 
+	
+	/* Typed subtitle */
+	$('.typed-title').typed({
+		stringsElement: $('.typing-title'),
+		backDelay: 2000,
+		typeSpeed: 60,
+		loop: true
+	});
+
+})(jQuery);
